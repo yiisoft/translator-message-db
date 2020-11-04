969d0ea84cc1111747116de3c479d419e2edf270
DarkDef <darkdef@gmail.com>
2020-11-04 18:37:15 +0300

    first look on refactoring

---
+++ src/MessageSource.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Translator\Message\Db;
+
+use Yiisoft\Arrays\ArrayHelper;
+use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Db\Expression\Expression;
+use Yiisoft\Db\Query\Query;
+use Yiisoft\Translator\MessageReaderInterface;
+use Yiisoft\Translator\MessageWriterInterface;
+
+final class MessageSource implements MessageReaderInterface, MessageWriterInterface
+{
+    private array $messages = [];
+    private ConnectionInterface $db;
+    private string $sourceMessageTable = '{{%source_message}}';
+    private string $messageTable = '{{%message}}';
+
+    public function __construct(ConnectionInterface $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getMessage(string $id, string $category, string $locale, array $parameters = []): ?string
+    {
+        if (!isset($this->messages[$category][$locale])) {
+            $this->read($category, $locale);
+        }
+
+        return $this->messages[$category][$locale][$id] ?? null;
+    }
+
+    private function read(string $category, string $locale): void
+    {
+        $query = (new Query($this->db))
+            ->select(['message_id', 'translation'])
+            ->from(['ts' => $this->sourceMessageTable])
+            ->innerJoin(['td' => $this->messageTable],
+                [
+                    'td.id' => new Expression('[[ts.id]]'),
+                    'ts.category' => $category
+                ])
+            ->where([
+                'locale' => $locale,
+            ]);
+        $messages = $query->all();
+
+        $this->messages[$category][$locale] = ArrayHelper::map($messages, 'message_id', 'translation');
+    }
+
+    private function clean(string $category, string $locale): void
+    {
+        unset($this->messages[$category][$locale]);
+    }
+
+    public function setSourceMessageTable(string $sourceMessageTable): void
+    {
+        $this->sourceMessageTable = $sourceMessageTable;
+    }
+
+    public function setMessageTable(string $messageTable): void
+    {
+        $this->messageTable = $messageTable;
+    }
+
+    public function write(string $category, string $locale, array $messages): void
+    {
+        $sourceMessages = (new Query($this->db))
+            ->select(['id', 'message_id'])
+            ->from($this->sourceMessageTable)
+            ->where(['category' => $category])
+            ->all();
+
+        $sourceMessages = ArrayHelper::map($sourceMessages, 'message_id', 'id');
+
+        $this->read($category, $locale);
+
+        foreach ($messages as $message_id => $translation) {
+            if (!isset($sourceMessages[$message_id])) {
+                $result = $this->db->getSchema()->insert($this->sourceMessageTable, ['category' => $category, 'message_id' => $message_id]);
+                if ($result===false)
+                    throw new \RuntimeException('Can not create source message with id ' . $message_id);
+                $sourceMessages[$message_id] = $result['id'];
+            }
+
+            $needUpdate = false;
+            if (isset($this->messages[$category][$locale][$message_id]) && $this->messages[$category][$locale][$message_id] !== $translation) {
+                $this->db->createCommand()->delete($this->messageTable, ['id' => $sourceMessages[$message_id]])->execute();
+                $needUpdate = true;
+            }
+
+            if ($needUpdate || !isset($this->messages[$category][$locale][$message_id])) {
+                $result = $this->db->getSchema()->insert($this->messageTable, ['id' => $sourceMessages[$message_id], 'locale' => $locale, 'translation' => $translation]);
+                if ($result===false)
+                    throw new \RuntimeException('Can not create source message with id ' . $message_id);
+            }
+        }
+
+        $this->clean($category, $locale);
+    }
+}
---
+++ tests/Migration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Yiisoft\Translator\Message\Db\Tests;
+
+use Yiisoft\Yii\Db\Migration\Migration as BaseMigration;
+
+/**
+ * Stub Migration class
+ * @package Yiisoft\Yii\Db\Migration\Tests
+ */
+class Migration extends BaseMigration
+{
+    public function up(): void
+    {
+    }
+
+    public function down(): void
+    {
+    }
+}
