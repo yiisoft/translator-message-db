0a94b7add300e3820b936c52296aeb575d8601cb
Valerii Gorbachev <darkdef@gmail.com>
2021-02-08 23:20:10 +0300

    Update dev dependencies, migrations and tests

--- src/migrations/M201104110256CreateMessageSource.php
+++ src/migrations/M201104110256CreateMessageSource.php
@@ -4,50 +4,50 @@ declare(strict_types=1);
 
 namespace Yiisoft\Translator\Message\Db\migrations;
 
-use Yiisoft\Yii\Db\Migration\Migration;
+use Yiisoft\Yii\Db\Migration\MigrationBuilder;
 use Yiisoft\Yii\Db\Migration\RevertibleMigrationInterface;
 
 /**
  * Class M201104110256CreateMessageSource
  */
-final class M201104110256CreateMessageSource extends Migration implements RevertibleMigrationInterface
+final class M201104110256CreateMessageSource implements RevertibleMigrationInterface
 {
-    public function up(): void
+    public function up(MigrationBuilder $b): void
     {
         $tableOptions = null;
 
-        if ($this->db->getDriverName() === 'mysql') {
+        if ($b->getDb()->getDriverName() === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%source_message}}', [
-            'id' => $this->primaryKey(),
-            'category' => $this->string(),
-            'message_id' => $this->text(),
-            'comment' => $this->text(),
+        $b->createTable('{{%source_message}}', [
+            'id' => $b->primaryKey(),
+            'category' => $b->string(),
+            'message_id' => $b->text(),
+            'comment' => $b->text(),
         ], $tableOptions);
 
-        $this->createTable('{{%message}}', [
-            'id' => $this->integer()->notNull(),
-            'locale' => $this->string(16)->notNull(),
-            'translation' => $this->text(),
+        $b->createTable('{{%message}}', [
+            'id' => $b->integer()->notNull(),
+            'locale' => $b->string(16)->notNull(),
+            'translation' => $b->text(),
         ], $tableOptions);
 
-        $this->addPrimaryKey('pk_message_id_locale', '{{%message}}', ['id', 'locale']);
+        $b->addPrimaryKey('pk_message_id_locale', '{{%message}}', ['id', 'locale']);
         $onUpdateConstraint = 'RESTRICT';
-        if ($this->db->getDriverName() === 'sqlsrv') {
+        if ($b->getDb()->getDriverName() === 'sqlsrv') {
             // 'NO ACTION' is equivalent to 'RESTRICT' in MSSQL
             $onUpdateConstraint = 'NO ACTION';
         }
-        $this->addForeignKey('fk_message_source_message', '{{%message}}', 'id', '{{%source_message}}', 'id', 'CASCADE', $onUpdateConstraint);
-        $this->createIndex('idx_source_message_category', '{{%source_message}}', 'category');
-        $this->createIndex('idx_message_locale', '{{%message}}', 'locale');
+        $b->addForeignKey('fk_message_source_message', '{{%message}}', 'id', '{{%source_message}}', 'id', 'CASCADE', $onUpdateConstraint);
+        $b->createIndex('idx_source_message_category', '{{%source_message}}', 'category');
+        $b->createIndex('idx_message_locale', '{{%message}}', 'locale');
     }
 
-    public function down(): void
+    public function down(MigrationBuilder $b): void
     {
-        $this->dropForeignKey('fk_message_source_message', '{{%message}}');
-        $this->dropTable('{{%message}}');
-        $this->dropTable('{{%source_message}}');
+        $b->dropForeignKey('fk_message_source_message', '{{%message}}');
+        $b->dropTable('{{%message}}');
+        $b->dropTable('{{%source_message}}');
     }
 }
