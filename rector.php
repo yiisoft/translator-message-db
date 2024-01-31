598c6a9ebe6135550e3488d89e77e9094206c781
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2024-01-31 07:19:25 -0300

    Update github actions, update config rector, psalm, add support php 8.3. (#67)

--- rector.php
+++ rector.php
@@ -4,31 +4,28 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    // paths
-    $rectorConfig->paths(
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-        ],
-    );
-
-    // skip paths
-    $rectorConfig->skip(
-        [
-            __DIR__ . '/config',
-        ],
-    );
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    $rectorConfig->sets(
-        [
-            LevelSetList::UP_TO_PHP_80,
-        ],
-    );
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_80,
+    ]);
+
+    $rectorConfig->skip([
+        ClosureToArrowFunctionRector::class,
+        JsonThrowOnErrorRector::class,
+        ReadOnlyPropertyRector::class,
+    ]);
 };
